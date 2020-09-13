9c23c1956097574e92214dbabfcc5bfdcaff8a83
rorygee <rorygee@outlook.com>
2020-09-13 15:30:57 +0100

    Implementing xp record insertion/updating in the newer DB handler module

--- app/dbHandler2.py
+++ app/dbHandler2.py
@@ -2,17 +2,19 @@
 
 import datetime
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, update
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from app import requestsHandler
 
+# Database Handler (SQLAlchemy/MySQL) #
 
-# Database Handler (SQLAlchemy/MySQL)
 
 # Establish a 'global' connection, need to modify to use a mysql driver
 engine = create_engine('sqlite:///:memory:', echo=True)
+
+session = sessionmaker(bind=engine)
 
 # Define a single instance to store references to database classes and tables
 Base = declarative_base()
@@ -81,7 +83,9 @@ def query_commit(query):
 
 
 def initialise_user_table():
+
     return 1
+
 def check_day(username,date):
 
     return 1
@@ -106,12 +110,66 @@ def add_xp_record(username):
         for x in range(24):
             print(xpData[x])
 
+        session.add(XpRecords(username=username, day=datetime.datetime.now(),
+                              Overall=xpData[0],
+                              Attack=xpData[1],
+                              Defence=xpData[2],
+                              Strength=xpData[3],
+                              Hitpoints=xpData[4],
+                              Ranged=xpData[5],
+                              Prayer=xpData[6],
+                              Magic=xpData[7],
+                              Cooking=xpData[8],
+                              Woodcutting=xpData[9],
+                              Fletching=xpData[10],
+                              Fishing=xpData[11],
+                              Firemaking=xpData[12],
+                              Crafting=xpData[13],
+                              Smithing=xpData[14],
+                              Mining=xpData[15],
+                              Herblore=xpData[16],
+                              Agility=xpData[17],
+                              Thieving=xpData[18],
+                              Slayer=xpData[19],
+                              Farming=xpData[20],
+                              Runecrafting=xpData[21],
+                              Hunter=xpData[22],
+                              Construction=xpData[23]))
+
     else:
-        return 1
+
+        replacedRecord = session.query(XpRecords).filter_by(username=username, day=datetime.datetime.now()).first()
+
+        session.add(XpRecords(replacedRecord(
+                              Overall=xpData[0],
+                              Attack=xpData[1],
+                              Defence=xpData[2],
+                              Strength=xpData[3],
+                              Hitpoints=xpData[4],
+                              Ranged=xpData[5],
+                              Prayer=xpData[6],
+                              Magic=xpData[7],
+                              Cooking=xpData[8],
+                              Woodcutting=xpData[9],
+                              Fletching=xpData[10],
+                              Fishing=xpData[11],
+                              Firemaking=xpData[12],
+                              Crafting=xpData[13],
+                              Smithing=xpData[14],
+                              Mining=xpData[15],
+                              Herblore=xpData[16],
+                              Agility=xpData[17],
+                              Thieving=xpData[18],
+                              Slayer=xpData[19],
+                              Farming=xpData[20],
+                              Runecrafting=xpData[21],
+                              Hunter=xpData[22],
+                              Construction=xpData[23])))
 
 
 def get_xp_records_for_user(username):
-
+    for user in session.query():
+        print(1)
     return 1
 
 
